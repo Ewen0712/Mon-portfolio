72ee1739d278f631353df566bf5990e11b921bde
Ewen0712 <ewen.coste0712@gmail.com>
2026-01-22 12:20:25 +0100

    Premier projets

--- projects.php
+++ projects.php
@@ -11,13 +11,13 @@
                 <div class="card h-100 p-0">
                     <!-- Bloc Titre -->
                     <div class="bg-primary text-white text-center py-2">
-                        <h5 class="mb-0">Projet 1 : Gestion de parc informatique</h5>
+                        <h5 class="mb-0">Projet 1 : Création, configuration et utilisation d’un outil de gestion du Parc Informatique : GLPI</h5>
                     </div>
                     <!-- Bloc Description -->
                     <div class="p-3">
-                        <p>Application pour gérer les utilisateurs et équipements d’une entreprise.</p>
+                        <p>Utilisation de GLPI pour la gestion efficace des utilisateurs et du matériel informatique de l'entreprise, et pour la gestion des demandes d'assistance.</p>
                     </div>
-                    <img src="assets/images/ram_prices.jpg" class="card-img-top" alt="Projet1">
+                    <img src="assets/fichier/image/Projets/GLPI.gif" class="card-img-top" alt="Projet1">
                     <!-- Bloc Documentation -->
                     <div class="p-3 text-center">
                         <a href="assets/fichier/pdf/projet1_doc.pdf" download class="btn btn-primary">Documentation</a>
@@ -29,12 +29,12 @@
             <div class="col-md-6">
                 <div class="card h-100 p-0">
                     <div class="bg-primary text-white text-center py-2">
-                        <h5 class="mb-0">Projet 2 : Site vitrine</h5>
+                        <h5 class="mb-0">Projet 2 : Mise en place d’un service DHCP Linux</h5>
                     </div>
                     <div class="p-3">
-                        <p>Site web créé pour présenter une entreprise fictive avec HTML/CSS/JS.</p>
+                        <p>Serveur Linux servant à la distribution automatique de l'adressage IP.</p>
                     </div>
-                    <img src="assets/images/ram_prices.jpg" class="card-img-top" alt="Projet2">
+                    <img src="assets/fichier/image/Projets/DHCP.jpg" class="card-img-top" alt="Projet2">
                     <div class="p-3 text-center">
                         <a href="assets/fichier/pdf/projet2_doc.pdf" download class="btn btn-primary">Documentation</a>
                     </div>
@@ -45,12 +45,12 @@
             <div class="col-md-6">
                 <div class="card h-100 p-0">
                     <div class="bg-primary text-white text-center py-2">
-                        <h5 class="mb-0">Projet 3 : Application de suivi de tickets</h5>
+                        <h5 class="mb-0">Projet 3 : Sécurisation du réseau avec un serveur proxy sur Linux : Squid</h5>
                     </div>
                     <div class="p-3">
-                        <p>Application permettant de créer et suivre des tickets pour le support informatique.</p>
+                        <p>Serveur Linux utilisant Squid comme proxy pour filtrer et contrôler les requêtes HTTP/HTTPS des utilisateurs.</p>
                     </div>
-                    <img src="assets/images/ram_prices.jpg" class="card-img-top" alt="Projet3">
+                    <img src="assets/fichier/image/Projets/SQUID.png" class="card-img-top" alt="Projet3">
                     <div class="p-3 text-center">
                         <a href="assets/fichier/pdf/projet3_doc.pdf" download class="btn btn-primary">Documentation</a>
                     </div>
@@ -58,6 +58,104 @@
             </div>
 
             <!-- Projet 4 -->
+            <div class="col-md-6">
+                <div class="card h-100 p-0">
+                    <div class="bg-primary text-white text-center py-2">
+                        <h5 class="mb-0">Projet 4 : Base de données entreprise</h5>
+                    </div>
+                    <div class="p-3">
+                        <p>Création d’une base de données relationnelle pour la gestion du personnel et du matériel.</p>
+                    </div>
+                    <img src="assets/images/ram_prices.jpg" class="card-img-top" alt="Projet4">
+                    <div class="p-3 text-center">
+                        <a href="assets/fichier/pdf/projet4_doc.pdf" download class="btn btn-primary">Documentation</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Projet 5 -->
+            <div class="col-md-6">
+                <div class="card h-100 p-0">
+                    <div class="bg-primary text-white text-center py-2">
+                        <h5 class="mb-0">Projet 5 : Mise en place d’un suivi de mes candidatures aux alternances et de l’automatisation de ce suivi </h5>
+                    </div>
+                    <div class="p-3">
+                        <p>Tableau Excel dynamique allié avec une tâche automatisée.</p>
+                    </div>
+                    <div style="max-height: 250px; overflow: hidden; text-align: center;">
+                        <img src="assets/fichier/image/Projets/EXCEL_PS.png" class="card-img-top" alt="Projet4" style="width: auto; height: auto;">
+                    </div>
+                    <div class="p-3 text-center">
+                        <a href="assets/fichier/pdf/projet4_doc.pdf" download class="btn btn-primary mt-5">Documentation</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Projet 6 -->
+            <div class="col-md-6">
+                <div class="card h-100 p-0">
+                    <div class="bg-primary text-white text-center py-2">
+                        <h5 class="mb-0">Projet 4 : Base de données entreprise</h5>
+                    </div>
+                    <div class="p-3">
+                        <p>Création d’une base de données relationnelle pour la gestion du personnel et du matériel.</p>
+                    </div>
+                    <img src="assets/images/ram_prices.jpg" class="card-img-top" alt="Projet4">
+                    <div class="p-3 text-center">
+                        <a href="assets/fichier/pdf/projet4_doc.pdf" download class="btn btn-primary">Documentation</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Projet 7 -->
+            <div class="col-md-6">
+                <div class="card h-100 p-0">
+                    <div class="bg-primary text-white text-center py-2">
+                        <h5 class="mb-0">Projet 4 : Base de données entreprise</h5>
+                    </div>
+                    <div class="p-3">
+                        <p>Création d’une base de données relationnelle pour la gestion du personnel et du matériel.</p>
+                    </div>
+                    <img src="assets/images/ram_prices.jpg" class="card-img-top" alt="Projet4">
+                    <div class="p-3 text-center">
+                        <a href="assets/fichier/pdf/projet4_doc.pdf" download class="btn btn-primary">Documentation</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Projet 8 -->
+            <div class="col-md-6">
+                <div class="card h-100 p-0">
+                    <div class="bg-primary text-white text-center py-2">
+                        <h5 class="mb-0">Projet 4 : Base de données entreprise</h5>
+                    </div>
+                    <div class="p-3">
+                        <p>Création d’une base de données relationnelle pour la gestion du personnel et du matériel.</p>
+                    </div>
+                    <img src="assets/images/ram_prices.jpg" class="card-img-top" alt="Projet4">
+                    <div class="p-3 text-center">
+                        <a href="assets/fichier/pdf/projet4_doc.pdf" download class="btn btn-primary">Documentation</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Projet 9 -->
+            <div class="col-md-6">
+                <div class="card h-100 p-0">
+                    <div class="bg-primary text-white text-center py-2">
+                        <h5 class="mb-0">Projet 4 : Base de données entreprise</h5>
+                    </div>
+                    <div class="p-3">
+                        <p>Création d’une base de données relationnelle pour la gestion du personnel et du matériel.</p>
+                    </div>
+                    <img src="assets/images/ram_prices.jpg" class="card-img-top" alt="Projet4">
+                    <div class="p-3 text-center">
+                        <a href="assets/fichier/pdf/projet4_doc.pdf" download class="btn btn-primary">Documentation</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Projet 10 -->
             <div class="col-md-6">
                 <div class="card h-100 p-0">
                     <div class="bg-primary text-white text-center py-2">
